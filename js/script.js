9f0f02a643b22076b9f12cd6f45f109b060d0a63
angelo friello <angelofriello01@gmail.com>
2023-10-16 16:13:32 +0200

    fix ciclo whild

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ const gridDOMElement = document.querySelector('.grid');
 
 playBtnDOMElement.addEventListener('click', function (){
 
+    const bombs = getArrayRandom (1, 100 ,16)
+    console.log(bombs)
+
     gridDOMElement.innerHTML = '';
     gridDOMElement.classList.remove('grid-hard');
     gridDOMElement.classList.remove('grid-mid');
-
+// selettore di difficoltà
+    // facile
     if(selectDOMElement.value == 0){
         gridDOMElement.classList.add('grid-easy');
         const gridEasyDOMElement = document.querySelector('.grid-easy');
@@ -15,6 +19,7 @@ playBtnDOMElement.addEventListener('click', function (){
             n = i;
             gridEasyDOMElement.innerHTML += `<div class="cell ">${n}</div>`;
         }
+    // medio
     }else if(selectDOMElement.value == 1){
         gridDOMElement.classList.add('grid-mid');
         const gridMidDOMElement = document.querySelector('.grid-mid');     
@@ -22,6 +27,7 @@ playBtnDOMElement.addEventListener('click', function (){
             n = i;
             gridMidDOMElement.innerHTML += `<div class="cell ">${n}</div>`;
         }
+    // difficile
     }else if(selectDOMElement.value == 2){
         gridDOMElement.classList.add('grid-hard');
         const gridHardDOMElement = document.querySelector('.grid-hard');
@@ -30,18 +36,29 @@ playBtnDOMElement.addEventListener('click', function (){
             gridHardDOMElement.innerHTML += `<div class="cell ">${n}</div>`;
         }
     }
-
+// lesezionare cella singola
     const cellDOMElements = document.querySelectorAll('.cell');
     console.log(cellDOMElements);
+// creare un ciclo per ciclare tutte le celle per poter selezionare le singole celle e darle degli atributi
+    for (let i = 0; i < cellDOMElements.length + 1; i++){
 
-    for (let i = 0; i < cellDOMElements.length; i++){
         const currentCellDOMElement = cellDOMElements[i];
+
         currentCellDOMElement.addEventListener('click', function() {
-            currentCellDOMElement.classList.add('bg-azurro');
-        console.log('hai selezionato la casella'+ `${i+1}`);``
+            
+            if(cellDOMElements[i + 1] === bombs.includes(n)){
+
+                currentCellDOMElement.classList.add('bg-red')
+                
+            }else{
+                currentCellDOMElement.classList.add('bg-azurro');
+            }
+            
+        console.log('hai selezionato la casella'+ `${i +1}`);
         })
     }
 })
+
 
 
 function getArrayRandom (minRange, maxRange, number) {
@@ -49,19 +66,15 @@ function getArrayRandom (minRange, maxRange, number) {
     const arrayBombs = []
     // console.log(arrayBombs)
 
-    while (arrayBombs.length > 16) {
-
-        const n = getRandomIntInclusive (1,100)
-
+    while (arrayBombs.length < number) { 
+        const n = getRandomIntInclusive (minRange,maxRange)
         // console.log(n)
 
         if (arrayBombs.includes(n) === false){
 
             arrayBombs.push(n)
         }
-        
     }
-    console.log(arrayBombs)
     return  arrayBombs
 }
 
